ba89756325f9c9e59f9ba2fd28eba81404e75237
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2020-10-02 19:13:34 +0300

    feat: add max min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
-exports.min = function min (array) {
-  return 0;
+
+exports.min = function min(array) {
+	if (array) {
+		if (array.length) {
+			return Math.min.apply(null, array);
+		} else {
+			return 0;
+		}
+	} else {
+		return 0;
+	}
 }
 
-exports.max = function max (array) {
-  return 0;
+
+
+exports.max = function max(array) {
+	if (array) {
+		if (array.length) {
+			return Math.max.apply(null, array);
+		} else {
+			return 0;
+		}
+	} else {
+		return 0;
+	}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (array) {
+		if (array.length) {
+			let result = array.reduce((x, y) => {
+				return x + y
+			})
+			return result / array.length;
+		} else {
+			return 0;
+		}
+	} else {
+		return 0;
+	}
 }
+
